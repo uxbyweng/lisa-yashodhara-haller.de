20d10f2629db51e74470df0ccd83a9f20e20f349
Karsten Weng <karsten@weng.eu>
2026-05-08 15:18:25 +0200

    Store YouTube thumbnails locally for GDPR-compliant facade

--- components/youtube_video.php
+++ components/youtube_video.php
@@ -2,7 +2,7 @@
 function youtube_video($youtube_id, $start_time = 0) {
     $id    = htmlspecialchars($youtube_id, ENT_QUOTES, 'UTF-8');
     $start = (int) $start_time;
-    echo '<div class="yt-facade video-banner" data-yt-id="' . $id . '" data-yt-start="' . $start . '" role="button" tabindex="0" aria-label="Video abspielen">';
+    echo '<div class="yt-facade video-banner" data-yt-id="' . $id . '" data-yt-start="' . $start . '" role="button" tabindex="0" aria-label="Video abspielen" style="background: url(/images/yt-thumbs/' . $id . '.jpg) no-repeat center; background-size: cover;">';
     echo   '<div class="yt-facade__notice" style="display:none">';
     echo     '<p>Dieses Video wird von YouTube bereitgestellt. Beim Abspielen werden Daten (u.&thinsp;a. Ihre IP-Adresse) an YouTube&thinsp;/&thinsp;Google &uuml;bertragen. Mehr in der <a href="/privacy/">Datenschutzerkl&auml;rung</a>.</p>';
     echo     '<button class="yt-facade__accept">Zustimmen &amp; abspielen</button>';
